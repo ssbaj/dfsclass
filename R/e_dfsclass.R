db2b2e8c65f3f9dda6405de51e2c397fc56f0f2a
ssbaj <ssb9018@ajou.ac.kr>
2026-01-22 16:36:41 +0900

    first commit

--- R/e_dfsclass.R
+++ R/e_dfsclass.R
@@ -1,9 +1,9 @@
-e_dfclass<-function(explaining=0){
+e_dfsclass<-function(explaining=0){
 
   if(explaining==0) {
   cat("  ", '\n')
   cat("\033[1;32m## Statistics for PA and AI dataset ----------- \033[0m", '\n')
-  cat("  행정통계와 AI 데이터셋 ", '\n')
+  cat("  Dataset ---- ", '\n')
   cat("  df00_id_brand_conv ", '\n')
   cat("  df01_study_exhaust ", '\n')
   cat("  df02_gdp_local_tax ", '\n')
