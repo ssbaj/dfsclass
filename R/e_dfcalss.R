488b1e780a136003ef19fd1b68c35d0466b99e67
ssbaj <ssb9018@ajou.ac.kr>
2026-01-22 13:53:46 +0900

    first commit

---
+++ R/e_dfcalss.R
@@ -0,0 +1,25 @@
+e_dfclass<-function(explaining=0){
+
+  if(explaining==0) {
+    cat("  ", '\n')
+    cat("\033[1;32m## Statistics for PA and AI dataset ----------- \033[0m", '\n')
+    cat("  행정통계와 AI 데이터셋 ", '\n')
+   cat("  df0: 자료1.csv ", '\n')
+   cat("  df1: 학업탈진 ", '\n')
+   cat("  df2: GDP성장률과 지방세 성장률 ", '\n')
+   cat("  df3: 감정노동자 서베이 데이터 ", '\n')
+   cat("  df4: GDP, 소비, Lag소비 ", '\n')
+   cat("  df5: 타이타닉 ", '\n')
+   cat("  df6: 2018년 가계금융복지 ", '\n')
+   cat("  df7: 카페 서베이 ", '\n')
+   cat("  df8: 2017년 사업체 패널조사 ", '\n')
+   cat("  df9: 국내여행 서베이 ", '\n')
+   cat("  df10: 2022년 주거실태조사 ", '\n')
+   cat("  df11: 2021년 NGO기부금자료 ", '\n')
+   cat("  df12: 기부행위에 대한 서베이조사 ", '\n')
+   cat("  df13: 기말시험대비 문제 - mdis ", '\n')
+   cat("  df14: 기말시험대비 문제 - NGO", '\n')
+   cat("  df15: suwond city Rev data", '\n')
+   cat("   ", '\n')
+  } }
+
